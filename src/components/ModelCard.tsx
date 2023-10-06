d5b51d0334970efa2e67d1ac8477a31006fb73fb
ArcAnya <arcanyatwitch@gmail.com>
2023-10-06 21:44:30 +0200

    model design details

--- src/components/ModelCard.tsx
+++ src/components/ModelCard.tsx
@@ -1,14 +1,54 @@
 export function ModelCard({
-    title
+    title,
+    usage,
+    size,
+    license,
+    downloads,
+    allowed,
 }: {
     title: string;
+    usage: string;
+    size: string;
+    license: string;
+    downloads: string;
+    allowed: boolean;
 }) {
+    const divStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      };
+    
+    const notAllowed = {
+        color: 'red',
+        padding: '10px',
+        border: '1px solid red',
+        borderRadius: '5px',
+      };
+
     return (
-        <div className="model">
-        <h1>{title}</h1>	
-        <div>
-            Details
-        </div>
+        <div className={`${allowed ? "model" : "modelNotAllowed"}`}>
+            <h1>{title}</h1>	
+            <div style={divStyle}>
+                <div >
+                    <div>
+                        Usage: {usage}
+                    </div>
+                    <div>
+                        Size: {size}
+                    </div>
+                    <div>
+                        License: {license}
+                    </div>
+                    <div>Downloads: {downloads}</div>
+                </div>
+                {!allowed && <div style={notAllowed}>Not accessible</div>}
+                
+                {/* <div style={{ fill: 'red' }}>
+                <img src={DownloadIcon} alt="DowloadIcon" className='octicon' />
+                    
+                </div> */}
+            </div>
       </div>
     );
 }
--- src/components/Models.tsx
+++ src/components/Models.tsx
@@ -7,9 +7,9 @@ export function Models() {
         <Sidebar />
         <div className="mainApp">
         <h1>Available AI Models</h1>	
-        <div>
-            <ModelCard title="Model 1" />
-            <ModelCard title="Model 2"/>
+        <div className="models">
+            <ModelCard title="Model 1" usage="Sentiment Analysis" size="1.2 GB" license="Open Source" downloads="25" allowed={true}/>
+            <ModelCard title="Model 2" usage="Price Prediction" size="2.4 GB" license="Propietary" downloads="5" allowed={false}/>
         </div>
         <div>
             <input type="text" placeholder="Model Name" />
