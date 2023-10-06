7992d9b0b6e0d00386cce816c673f690c9453235
Maurits Bos <mb.brains@gmail.com>
2023-10-06 22:38:18 +0300

    improved bash script for program

--- rules/3_get_last_stored_rules_alt.sh
+++ rules/3_get_last_stored_rules_alt.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-okp4d query tx --node https://api.testnet.okp4.network:443/rpc $STORED_TRANSACTION_HASH --output json | jq  '.program' 
+okp4d query tx --node https://api.testnet.okp4.network:443/rpc $STORED_TRANSACTION_HASH --output json | jq  '.tx.body.messages[0].msg.program' | tr -d '"' | base64 -d
