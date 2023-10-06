5a61a96b935b9d0a6b810eb9ebb8800cccb733ed
Vaughn Hew <vaughn.hew@gmail.com>
2023-10-07 01:12:57 +0300

    deploy blocktrain-runner

--- env.staging.blocktrain.sh
+++ env.staging.blocktrain.sh
@@ -4,7 +4,7 @@ export DEBUG=DEBUG,INFO,WARN,ERROR
 export ADDRESS="okp41qljavkuszlclekhd2mjrnaexp4l6aljtnrvk6u"
 
 # docker
-export IMAGE=ghcr.io/okp4ai
+export IMAGE=ghcr.io/blocktrainbuilders
 export TAG=v`node -pe "require(\"./package.json\")['version']"`
 
 # AKash private
