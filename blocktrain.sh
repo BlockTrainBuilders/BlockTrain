4288d4a2789794833bf80765ba7d203630997a06
Vaughn Hew <vaughn.hew@gmail.com>
2023-10-07 09:29:23 +0300

    removed unecessary app now we have TrainingApp

--- blocktrain.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "replace me with node or whatever"
-echo "1. query OKP4 and get data URL and other params"
-echo "2. retrieve the data"
-echo "3. build the model"
-echo "4. make an API available to query the model and other OKP4 values"
-
